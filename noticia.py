fc1e0ca14aed38bbf1bc83a770cf5b8037cc66cf
Rodolpho Gomes <rodolphomalheiros@protonmail.com>
2021-05-02 00:18:26 -0300

    projeto pronto sem MVC

--- noticia.py
+++ noticia.py
@@ -1,11 +1,17 @@
+from estado import Estado
+
 class Noticia:
 
-    def __init__(self, video: str, imagem_show: str, titulo: str, conteudo: str, data: int):
+    def __init__(self, video: str, imagem_show: str, titulo: str, conteudo: str, data: int, estado, likes: int, views: int, id: int):
         self.__video = video
         self.__imagem_show = imagem_show
         self.__titulo = titulo
         self.__conteudo = conteudo
         self.__data = data
+        self.__estado = estado
+        self.__likes = likes
+        self.__views = views
+        self.__id = id
 
     def get_video(self) -> str:
         return self.__video
@@ -22,4 +28,14 @@ class Noticia:
     def get_data(self) -> int:
         return self.__data
 
-    
+    def get_estado(self):
+        return self.__estado
+
+    def get_likes(self) -> int:
+        return self.__likes
+
+    def get_views(self) -> int:
+        return self.__views
+
+    def get_id(self) -> int:
+        return self.__id
